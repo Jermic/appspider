885dc5f6cd1fac57ce3aa3394f8a510354ab3b91
ddvv <dadavivi512@gmail.com>
2018-05-26 11:29:45 +0800

    add README.md

--- appspider/pipelines.py
+++ appspider/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 import asyncpg
 import asyncio
 import copy
@@ -12,7 +13,15 @@ from appspider.configs.postgreconfig import *
 
 
 class AppspiderPipeline(object):
+    def open_spider(self, spider):
+        self.file = open(spider.name + '.txt', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
 
 
